eb0bcc1bf97cf28f8af212991f43e55bc5b0352b
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2024-12-04 04:55:40 +0700

    feat: add a link

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Logo() {
   return (
     <div>
-      <h1 className="text-2xl">Futer</h1>
+      <h1 className="text-2xl">
+        <Link href="/">Futer</Link>
+      </h1>
     </div>
   );
 }
