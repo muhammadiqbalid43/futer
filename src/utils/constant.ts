a70cf2db431e2f3d42250825137c022c122a5bdc
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2024-12-03 07:54:11 +0700

    feat: add images

---
+++ src/utils/constant.ts
@@ -0,0 +1,16 @@
+export const COLLECTION = [
+  {
+    sofa: "https://plus.unsplash.com/premium_photo-1661780295073-98db12600af0?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    bed: "https://plus.unsplash.com/premium_photo-1670076515907-2736a3492f23?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    table:
+      "https://plus.unsplash.com/premium_photo-1681412205470-77848a519359?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    chair:
+      "https://plus.unsplash.com/premium_photo-1705169612592-32610774a5d0?q=80&w=1440&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
