80f3c916dd46e6a5ce5cf01a41f6e04c8df77322
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2024-12-04 06:04:07 +0700

    feat add: breadcrumb component

---
+++ src/components/product-detail-breadcrumb.tsx
@@ -0,0 +1,26 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "./ui/breadcrumb";
+
+export default function ProductDetailBreadCrumb() {
+  return (
+    <div>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/product">Product</BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+}
